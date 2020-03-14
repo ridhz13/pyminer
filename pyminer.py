43e31656cddeba930967e560c737cd83c8ea0c95
ridhz13 <37190063+ridhz13@users.noreply.github.com>
2020-03-14 22:16:48 +0700

    Update pyminer.py

--- pyminer.py
+++ pyminer.py
@@ -24,7 +24,39 @@ import struct
 import re
 import base64
 import httplib
+import #!/usr/bin/python
+#
+# Copyright 2011 Jeff Garzik
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; see the file COPYING.  If not, write to
+# the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+
+import time
+import json
+import pprint
+import hashlib
+import struct
+import re
+import base64
+import httplib
 import sys
+from multiprocessing import Process
+
+ERR_SLEEP = 15
+MAX_NONCE = 1000000L
+
+settings = {}sys
 from multiprocessing import Process
 
 ERR_SLEEP = 15
